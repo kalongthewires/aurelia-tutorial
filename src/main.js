f7da4684eb4f005e8b0378dde956cdeced59608a
Kim Hamper <khamper+netjets@base2.io>
2017-08-14 15:40:21 -0400

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+// we want font-awesome to load as soon as possible to show the fa-spinner
+import '../static/styles.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'babel-polyfill';
+import 'material-components-web/';
+import 'material-components-web/dist/material-components-web.css';
+import * as Bluebird from 'bluebird';
+
+// remove out if you don't want a Promise polyfill (remove also from webpack.config.js)
+Bluebird.config({ warnings: { wForgottenReturn: false } });
+
+export async function configure(aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging()
+    .plugin(PLATFORM.moduleName('aurelia-mdc-plugin'));
+
+  // Uncomment the line below to enable animation.
+  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-animator-css'));
+  // if the css animator is enabled, add swap-order="after" to all router-view elements
+
+  // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  // aurelia.use.plugin(PLATFORM.moduleName('aurelia-html-import-template-loader'));
+
+  await aurelia.start();
+  await aurelia.setRoot(PLATFORM.moduleName('app'));
+}
